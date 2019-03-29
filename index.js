a83a0b10393843e7861e39bd82e49a28c7a10660
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-03-29 16:06:48 -0700

    Refacto users-model to ES6 syntax

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function restricted(req, res, next) {
   }
 }
 
-// GET all users
+//  GET - /api/users - If the user is logged in, respond with an array of all the users contained in the database. If the user is not logged in repond with the correct status code and the message: 'You shall not pass!'.   
 server.get('/api/users', restricted, async (req, res) => {
   try {
     const users = await UsersModel.find(); 
@@ -81,7 +81,6 @@ server.post('/api/register', (req, res) => {
     })
 })
 
-
 //  POST    /api/login     Use the credentials sent inside the `body` to authenticate the user. On successful login, create a new session for the user and send back a 'Logged in' message and a cookie that contains the user id. If login fails, respond with the correct status code and the message: 'You shall not pass!' 
 server.post('/api/login', (req, res) => {
   let { username, password } = req.body;
@@ -99,11 +98,6 @@ server.post('/api/login', (req, res) => {
       res.status(500).json({ error })
     })
 })
-
-
-
-//  GET     /api/users     If the user is logged in, respond with an array of all the users contained in the database. If the user is not logged in repond with the correct status code and the message: 'You shall not pass!'.   
-
 
 // Destroy the session
 server.get('/api/logout', (req, res) => {
--- users/users-model.js
+++ users/users-model.js
@@ -1,45 +1,22 @@
 const db = require('../data/dbConfig');
 
 module.exports = {
-  find,
-  findBy,
-  findById,
-  add
+  
+  find: () => {
+    db('users').select('id', 'username', 'password')
+  },
+  
+  findBy: filter => {
+    db('users').where(filter)
+  },
+  
+  findById: id => {
+    find().where({ id }).first()
+  },
+  
+  add: async user => {
+    const [ id ] = await db('users').insert(user);
+    return findById(id);
+  }
+  
 }
-
-function find() {
-  return db('users').select('id', 'username', 'password')
-};
-
-function findBy(filter) {
-  return db('users').where(filter)
-};
-
-function findById(id) {
-  return find().where({
-    id
-  }).first();
-}
-
-async function add(user) {
-  const [id] = await db('users').insert(user);
-  return findById(id);
-}
-// I can't figure out why the code below doesn't work
-// module.exports = {
-//   find,
-//   findById,
-//   add,
-//   findBy
-// }
-
-// const find = () => db('users').select('id', 'username', 'password');
-
-// const findBy = filter => db('users').where(filter);
-
-// const findById = id => find().where({ id }).first();
-
-// const add = async user => {
-//   const [ id ] = await db('users').insert(user);
-//   return findById(id);
-// }
