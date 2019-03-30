49d4d89bda4eae88c4b905585413ee5c70851304
Christene Bennett <christene@Christenes-MacBook-Pro.local>
2019-03-29 18:58:14 -0700

    Create React app; add ability to login from frontend

---
+++ client/user-app/src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      users: [],
+      user: {
+        username: '',
+        password: ''
+      }
+    }
+  }
+  // componentDidMount(){
+  //   axios
+  //     .get('http://localhost:9000/api/users')
+  //     .then(response => {
+  //       console.log(response)
+  //     })
+  //     .catch(err => console.log(err))
+  // }
+
+  onInputChange = event => {
+    this.setState({
+      user: {
+        ...this.state.user,
+        [event.target.name]: event.target.value
+      }   
+    })
+  }
+
+  onRegister = () => {
+    const newUser = this.state.user;
+    axios
+      .post('http://localhost:9000/api/register', newUser)
+      .then(response =>  console.log(response))
+      .catch(err => console.log(err))
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Welcome to the API!
+          <form>
+            <div>Register:</div>
+            <input type="text" name="username" placeholder="username" onChange={this.onInputChange}/>
+            <input type="text" name="password" placeholder="password" onChange={this.onInputChange}/>
+            <button onClick={this.onRegister}>Register</button>
+          </form>
+          <form>
+            <div>Login:</div>
+            <input type="text" name="username" placeholder="username" onChange={this.onInputChange}/>
+            <input type="text" name="password" placeholder="password" onChange={this.onInputChange}/>
+          </form>
+
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
--- users/users-model.js
+++ users/users-model.js
@@ -1,22 +1,49 @@
 const db = require('../data/dbConfig');
 
 module.exports = {
-  
-  find: () => {
-    db('users').select('id', 'username', 'password')
-  },
-  
-  findBy: filter => {
-    db('users').where(filter)
-  },
-  
-  findById: id => {
-    find().where({ id }).first()
-  },
-  
-  add: async user => {
-    const [ id ] = await db('users').insert(user);
-    return findById(id);
-  }
-  
+  find,
+  findBy,
+  findById,
+  add
 }
+
+function find() {
+  return db('users').select('id', 'username', 'password')
+};
+
+function findBy(filter) {
+  return db('users').where(filter)
+};
+
+function findById(id) {
+  return find().where({
+    id
+  }).first();
+}
+
+async function add(user) {
+  const [id] = await db('users').insert(user);
+  return findById(id);
+}
+
+
+// module.exports = {
+  
+//   find: () => {
+//     db('users').select('id', 'username', 'password')
+//   },
+  
+//   findBy: filter => {
+//     db('users').where(filter)
+//   },
+  
+//   findById: id => {
+//     find().where({ id }).first()
+//   },
+  
+//   add: async user => {
+//     const [ id ] = await db('users').insert(user);
+//     return findById(id);
+//   }
+
+// }
